5c65fb92817382093522ed73a457f027230485d0
Jane <janeparkjp@gmail.com>
2017-04-05 21:29:07 -0400

    jb rough draft

---
+++ jukebox.js
@@ -0,0 +1,30 @@
+//i know var Jukebox needs to be a singleton, since it's the function
+//that is used to play the actual jukebox, but i'm not too sure
+//how to go about it
+//also, i'm not too sure how to go about this, since not too sure how to
+//insert songs yet.
+//this is sooo bare bones. i don't know if i'm heading towards the right direction
+
+var Jukebox = {
+
+function playSong(){
+	song.play()
+},
+
+function pauseSong() {
+	song.pause()
+},
+
+function changeSong(new song){
+	song.change() {
+		}
+	}
+},
+};
+
+class song {
+	constructor (songFile) {
+
+	}
+}
+}
